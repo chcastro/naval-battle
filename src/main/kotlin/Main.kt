06f66eb68efb0502f8c39fcf11bd232d948f1415
Unknown <f.christian.castro@gmail.com>
2018-06-21 22:45:03 -0500

    API REST with json

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,34 @@
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.features.CallLogging
-import io.ktor.features.DefaultHeaders
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.GsonConverter
 import io.ktor.http.ContentType
-import io.ktor.response.respondText
-import io.ktor.routing.Routing
+import io.ktor.response.respond
 import io.ktor.routing.get
+import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.util.*
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080, watchPaths = listOf("BlogAppKt"), module = Application::module).start()
 }
 
+data class Snippet(val text: String)
+
+val snippets = Collections.synchronizedList(mutableListOf(
+        Snippet("hello"),
+        Snippet("world")
+))
+
 fun Application.module() {
-    install(DefaultHeaders)
-    install(CallLogging)
-    install(Routing) {
-        get("/") {
-            call.respondText("My Example Blog2", ContentType.Text.Html)
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, GsonConverter())
+    }
+    routing {
+        get("/snippets") {
+            call.respond(mapOf("snippets" to synchronized(snippets) { snippets.toList() }))
         }
     }
 }
