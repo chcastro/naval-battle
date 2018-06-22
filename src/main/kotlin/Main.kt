665376e45ec2d8c178dcacd29590f4332879edbf
Unknown <f.christian.castro@gmail.com>
2018-06-21 22:20:58 -0500

    Easy example

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080, watchPaths = listOf("BlogAppKt"), module = Application::module).start()
+}
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("My Example Blog2", ContentType.Text.Html)
+        }
+    }
+}
